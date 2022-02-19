cc48a5ea74917d06c858d2a7ab8dbe9bbd1c0b64
haddadbechir99 <haddadbechir99@icloud.com>
2022-02-19 14:53:34 +0100

    crud

---
+++ ONEPIECE-APESHOP/src/services/CommandeCRUD.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import entities.Commande;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.MyConnection;
+/**
+ *
+ * @author ASUS
+ */
+public class CommandeCRUD {
+     Connection cnx2 ; 
+    public CommandeCRUD (){
+        cnx2 = MyConnection.getInstance().getCnx();
+    }
+    
+    public void ajouterCommande(){
+        try {
+            String requete ="INSERT INTO commandes (dateCreation , dateEnvoi,numeroUtilisateur, statut) "
+                    + "VALUES ('24152020','26022022','123','bechir')"; 
+            Statement st =cnx2.createStatement() ;
+            st.executeUpdate(requete); 
+            System.out.println ("Commande ajoutée avec succés"); 
+        } catch (SQLException ex) {
+           System.err.println (ex.getMessage());
+        }
+    }
+    
+    public void ajouterCommande2 (Commande c ){
+        try {
+            String requete2 = "INSERT INTO commandes (dateCreation , dateEnvoi,numeroUtilisateur, statut) "
+                    + "VALUES (?,?,?,?)";
+            PreparedStatement pst = cnx2.prepareStatement(requete2);
+            pst.setString(1,c.getDateCreation());
+            pst.setString (2,c.getDateEnvoi());
+            pst.setInt (3,c.getNumeroUtilisateur());
+            pst.setString (4,c.getStatut()); 
+            pst.executeUpdate(); 
+            System.out.println (" votre Commande ajoutée avec succés"); 
+
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+    
+    public List<Commande> afficherCommandes (){
+                    List<Commande> myList = new ArrayList<>();
+        try {
+            String requete3 = " SELECT * FROM commandes ";
+            Statement st = cnx2.createStatement();
+            ResultSet rs= st.executeQuery(requete3);  
+            while (rs.next()){
+            Commande c = new Commande() ; 
+            c.setNumeroCommande(rs.getInt(1));
+            c.setDateCreation(rs.getString(2));
+            c.setDateEnvoi(rs.getString(3));
+            c.setNumeroUtilisateur(rs.getInt(4));
+            c.setStatut(rs.getString(5));
+            myList.add(c);
+            }
+            
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return myList ;
+    }
+    
+     public void modifierCommande (Commande c , int numeroCommande){
+    try {
+            String req = "UPDATE commandes SET dateCreation=?,dateEnvoi=?,numeroUtilisateur=?,statut=? "
+                    + "WHERE numeroCommande=?";
+            PreparedStatement pst = cnx2.prepareStatement(req);
+             pst.setString(1,c.getDateCreation());
+            pst.setString(2,c.getDateEnvoi());
+            pst.setInt (3,c.getNumeroUtilisateur()); 
+            pst.setString(4,c.getStatut()) ;
+             pst.setInt(5,c.getNumeroCommande()) ;
+            pst.executeUpdate();
+            System.out.println("commande modifie !");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+  
+   public void supprimerCommande(int numeroCommande){
+    try {
+            String req = "DELETE FROM commandes "
+                    + "WHERE numeroCommande=?";
+            PreparedStatement pst = cnx2.prepareStatement(req);
+            pst.setInt(1, numeroCommande);
+            pst.executeUpdate();
+            System.out.println("commande supprime !");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+}
---
+++ ONEPIECE-APESHOP/src/services/FactureCRUD.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import entities.Facture;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import utils.MyConnection;
+
+/**
+ *
+ * @author ASUS
+ */
+public class FactureCRUD {
+    Connection cnx2 ; 
+    public FactureCRUD (){
+        cnx2 = MyConnection.getInstance().getCnx();
+    }
+    
+    public void ajouterFacture(){
+        try {
+            String requete ="INSERT INTO factureS (typeLivraison , prixLivraison, numeroRegionDestinataire) "
+                    + "VALUES ('145il','12','352')"; 
+            Statement st =cnx2.createStatement() ;
+            st.executeUpdate(requete); 
+            System.out.println ("Facture ajoutée avec succés"); 
+        } catch (SQLException ex) {
+           System.err.println (ex.getMessage());
+        }
+    }
+
+ public void ajouterFacture2(Facture f ){
+        try {
+            String requete2 = "INSERT INTO factures (typeLivraison , prixLivraison, numeroRegionDestinataire) "
+                    + "VALUES (?,?,?)";
+            PreparedStatement pst = cnx2.prepareStatement(requete2);
+            pst.setString(1,f.getTypeLivraison());
+            pst.setFloat(2,f.getPrixLivraison());
+            pst.setInt (3,f.getNumeroRegionDestinataire()); 
+            pst.executeUpdate(); 
+            System.out.println (" votre Facture est  ajoutée avec succés"); 
+
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+  public List<Facture> afficherFactures (){
+                    List<Facture> myListf = new ArrayList<>();
+        try {
+            String requete3 = " SELECT * FROM factureS ";
+            Statement st = cnx2.createStatement();
+            ResultSet rs= st.executeQuery(requete3);  
+            while (rs.next()){
+           Facture f = new Facture() ; 
+            f.setNumeroFacture(rs.getInt(1));
+            f.setTypeLivraison(rs.getString(2));
+            f.setPrixLivraison(rs.getFloat(3));
+            f.setNumeroRegionDestinataire(rs.getInt(4));
+            myListf.add(f);
+            }
+            
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return myListf;
+    }
+    
+  public void modifierFacture (Facture f , int numeroFacture){
+    try {
+            String req = "UPDATE factures SET typeLivraison=?,prixLivraison=?,numeroRegionDestinataire=? "
+                    + "WHERE numeroFacture=?";
+            PreparedStatement pst = cnx2.prepareStatement(req);
+             pst.setString(1,f.getTypeLivraison());
+            pst.setFloat(2,f.getPrixLivraison());
+            pst.setInt (3,f.getNumeroRegionDestinataire()); 
+            pst.setInt(4,f.getNumeroFacture()) ; 
+            pst.executeUpdate();
+            System.out.println("facture modifie !");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+  
+   public void supprimerFacture(int numeroFacture){
+    try {
+            String req = "DELETE FROM factures  "
+                    + "WHERE numeroFacture=?";
+            PreparedStatement pst = cnx2.prepareStatement(req);
+            pst.setInt(1, numeroFacture);
+            pst.executeUpdate();
+            System.out.println("facture supprime !");
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+}
